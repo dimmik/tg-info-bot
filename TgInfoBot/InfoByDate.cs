7b1ed75f0d577bed4445044c7ee09523ada7b7cd
Dmitry Andrievsky <ad@dimmik.org>
2022-10-03 08:41:40 +0200

    formatting

--- TgInfoBot/InfoByDate.cs
+++ TgInfoBot/InfoByDate.cs
@@ -45,7 +45,7 @@
             if (before.Any()) 
             {
                 var item = before.First();
-                res += $"С предыдущего {DescriptionFromTo} прошло {((dateTime - item.to).TotalDays):0:##} д. ({dateTime}, {item.to})";
+                res += $"С предыдущего {DescriptionFromTo} прошло {((dateTime - item.to).TotalDays):0.##} д.";
             }
             return res;
         }
